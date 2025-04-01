479ae15d70ba27bb47f8e7413a91cb51d6e225b9
Marwan <marwanmo475@gmail.com>
2025-03-31 22:37:46 -0600

    mistake

--- socketAB.h
+++ socketAB.h
@@ -6,7 +6,7 @@ int CLOSE_Socket(int socket);
 int CLEANUP_Sockets();
 
 int UDP_Socket();
-int UDP_Sendto(int socket, int destport, char* destip);
+int UDP_Sendto(int socket, int destport, char* destip, char* sendbuf, int sendsize);
 int UDP_Bind(int socket, int port, char* interface);
 int UDP_Recvfrom(int socket, char* buff, int buffsize);
 
